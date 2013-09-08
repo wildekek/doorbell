9654da3b7a098e4d0cb157af87bc412176dd2d42
Willem Vooijs <willem@raspberrypi.(none)>
2013-09-08 17:52:48 +0200

    Moved config to ini file, added example

--- doorbell.py
+++ doorbell.py
@@ -1,9 +1,15 @@
 """
 This script triggers Prowl to send out a push notification
 """
+import ConfigParser
 import prowlpy
 
-apikey = '305209cbe316bdc8112635a11895a45c4ae1d70d'
+config = ConfigParser.ConfigParser()
+config.read('doorbell.ini')
+
+apikey = config.get("prowl", "apikey")
+print apikey
+
 p = prowlpy.Prowl(apikey)
 try:
     p.add('Doorbell','There is someone at the door!',"You could like open up for example", 1, None, 'http://192.168.192.5/gpio_status')
